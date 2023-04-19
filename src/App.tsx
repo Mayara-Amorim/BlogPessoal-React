d44a002e9783177139ef8c1e1ef90a72880db662
Mayara Amorim <101272224+Mayara-Amorim@users.noreply.github.com>
2023-04-19 15:36:36 -0300

    Rotas e Implementação de Login e CadstroUsuario

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Login from "./paginas/login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {Grid} from "@material-ui/core";
 import "./App.css";
+import CadastroUsuario from "./paginas/cadastroUsuario/CadastroUsuario";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Home />} />
-         {/*<Route path="/cadastro" element={<CadastroUsuario />} />*/}
+         <Route path="/cadastro" element={<CadastroUsuario />} />
         </Routes>
       </div>
       <Footer />
